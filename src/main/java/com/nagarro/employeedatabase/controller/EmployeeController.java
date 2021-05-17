e27797cefc8a1cbf4330419811fc841d48f0305e
Vaibhav Jain <64792363+VaibhavJain1908@users.noreply.github.com>
2021-05-17 11:11:02 +0530

    Add files via upload

--- src/main/java/com/nagarro/employeedatabase/controller/EmployeeController.java
+++ src/main/java/com/nagarro/employeedatabase/controller/EmployeeController.java
@@ -6,7 +6,6 @@ import com.nagarro.employeedatabase.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -32,8 +31,7 @@ public class EmployeeController {
 
     }
 
-    @GetMapping
-    @RequestMapping(value = "employees")
+    @GetMapping(value = "/employees")
     public List<Employee> employees() {
 
         List<Employee> employees = employeeService.list();
